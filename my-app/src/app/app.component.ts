27a7f683d9c8084caf937c8e8295f458455f7e18
Alvaro-Hernandez <alvaropineda606@gmail.com>
2022-10-30 21:42:24 -0600

    Uso y definicion de Property Binding

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -11,4 +11,11 @@ export class AppComponent {
   //Estando en el componente debemos crear un atributo que guarde la imagen
   //En este caso usaremos una direccion que genera imagenes aleartorias al recargar la pagina
   img = 'https://source.unsplash.com/random';
+  btnDisabled = true;
+  //Vamos a crear un objeto para poder modificar a nivel dinamico los atributos
+  persona ={
+    name : 'Marcia',
+    edad : 19,
+    img : 'https://source.unsplash.com/random'
+  }
 }
