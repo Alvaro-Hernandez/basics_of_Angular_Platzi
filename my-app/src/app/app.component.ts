291c19b1e7b3a46c44fcf467f691560dab6b96bc
Alvaro-Hernandez <alvaropineda606@gmail.com>
2023-02-01 11:12:57 -0600

    Repaso de Property Binding

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,21 +1,22 @@
 import { Component } from '@angular/core';
 
+//Decorador
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  name = 'Alvaro'; //👌Public
-  edad = 21;
+  name = 'Angie'; //👌Public
+  edad = 20;
   //Estando en el componente debemos crear un atributo que guarde la imagen
   //En este caso usaremos una direccion que genera imagenes aleartorias al recargar la pagina
   img = 'https://source.unsplash.com/random';
   btnDisabled = true;
   //Vamos a crear un objeto para poder modificar a nivel dinamico los atributos
   persona ={
-    name : 'Marcia',
-    edad : 99,
+    name : 'Alvaro',
+    edad : 100,
     img : 'https://source.unsplash.com/random'
   }
 
